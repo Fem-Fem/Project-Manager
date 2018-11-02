808bed9c31a5fd878ad96406888a2cd4fc24ade9
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-02 14:43:40 +0000

    Add paths to worker controller

---
+++ app/controllers/worker_controller.rb
@@ -0,0 +1,23 @@
+class WorkerController < ApplicationController
+
+  get '/workers/index' do
+  end
+
+  get '/workers/new' do
+  end
+
+  post '/workers/index' do
+  end
+
+  get '/workers/:id' do
+  end
+
+  get '/workers/:id/edit' do
+  end
+
+  patch '/workers/:id/edit' do
+  end
+
+  delete '/workers/:id/delete' do
+  end
+end
