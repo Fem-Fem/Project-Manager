3499dad5bb2a5d3e85a4751b7c991e321210d720
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-02 14:45:00 +0000

    Add paths to company controller

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,2 +1,41 @@
 class CompanyController < ApplicationController
+
+  get '/companies/index' do
+  end
+
+  get '/companies/new' do
+  end
+
+  post '/companies/index' do
+  end
+
+  get '/companies/:id' do
+  end
+
+  get '/companies/:id/edit' do
+  end
+
+  patch '/companies/:id/edit' do
+  end
+
+  delete '/companies/:id/delete' do
+  end
+
+  get '/login' do
+  end
+
+  post '/login' do
+  end
+
+  get '/signup' do
+  end
+
+  post '/signup' do
+  end
+
+  get '/logout' do
+  end
+
+  post '/logout' do
+  end
 end
