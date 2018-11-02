9d933d794943a06e84e841777963ec262471cf2e
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-02 13:45:33 +0000

    Add validations for company and project model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,7 @@
 class Company < ActiveRecord::Base
   has_many :workers
   has_many :projects, through: :workers
+  has_secure_password
+  validates_presence_of :name, :location, :password, :motto
+  validates :name, uniqueness: true
 end
