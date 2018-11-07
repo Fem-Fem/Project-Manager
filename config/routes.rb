88d0b6014b3795c3c6990febc631f8e0e1e504e8
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-07 22:53:09 +0000

    Add nested resource workers to projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   resources :companies, only: [:index, :show, :new, :edit, :account, :login, :signup, :update]
-  resources :projects, only: [:index, :show, :new, :edit, :update, :create]
+  resources :projects, only: [:index, :show, :new, :edit, :update, :create] do
+    resources :workers, only: [:index, :show]
+  end
   resources :workers, only: [:index, :show, :new, :edit, :delete, :update, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
