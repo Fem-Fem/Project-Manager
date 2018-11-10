cd785599658453adefd7978a6ae27b62148a59db
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-10 19:12:14 +0000

    Delete unnecessary comments

--- db/migrate/002_create_projects.rb
+++ db/migrate/002_create_projects.rb
@@ -4,46 +4,7 @@ class CreateProjects < ActiveRecord::Migration
       t.string :name
       t.string :description
       # boolean for status?
-      # t.datetime :start_date
-      # t.datetime :end_date
       t.timestamps null: false
     end
   end
 end
-
-# Companies are all working together for something
-#
-# project has many workers
-#
-# companies has many workers
-#
-# projects have many companies, through workers
-# companies have many projects, through workers
-
-#
-# tasks has many
-#
-# a has many b
-# c has many b
-# a has many c through b
-#
-#
-# Movie
-#
-# actors, movies, rolls
-#
-# Movies has many rolls
-# Movies has many actors, through rolls
-#
-# Actor has many rolls
-# Actor has many actor, through rolls
-#
-# rolls belongs to actor
-# rolls belongs to movie
-#
-#
-# cook has many recipes
-#
-#
-# recipes has many ingredients
-# cook has many ingredients through recipes
