eeb12a8ad575f9c948ee40ac63f7e5a306176b5d
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-10 18:50:11 +0000

    Fix signup issu

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,7 @@
 class CompaniesController < ApplicationController
   before_action :require_login
   before_action :already_logged_in, only: [:signup, :login, :account]
-  skip_before_action :require_login, only: [:require_login, :signup, :login, :account]
+  skip_before_action :require_login, only: [:require_login, :signup, :login, :account, :authenticate_signup]
 
   def account
     render :account
@@ -16,14 +16,15 @@ class CompaniesController < ApplicationController
   end
 
   def authenticate_signup
-    binding.pry
     @company = Company.new(name: params[:company][:name], password: params[:company][:password], location: params[:company][:location], motto: params[:company][:motto])
     if @company.valid?
       @company.save
       session[:name] = params[:company][:name]
       redirect_to company_path(@company)
     else
-      render :index
+      binding.pry
+      @errors = @company.errors.full_messages.to_sentence
+      render :'/companies/signup'
     end
   end
 
