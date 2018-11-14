a0b2d715ba7d09453c6441090a1b47f4374b0093
Obafemi Fadairo <obafad94@gmail.com>
2018-11-13 20:00:59 -0500

    Working on omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,31 +3,30 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if auth
-      @company = Company.new
-      @company.name = auth["info"]["raw_info"]["company"]
-      @company.location = auth["extra"]["raw_info"]["location"]
-      # change motto to bio?
-      # @company.bio = auth["extra"]["raw_info"]["bio"]
+    # if auth
+    #   @company = Company.new
+    #   @company.name = auth["info"]["raw_info"]["company"]
+    #   @company.location = auth["extra"]["raw_info"]["location"]
+    #   change motto to bio?
+    #   @company.bio = auth["extra"]["raw_info"]["bio"]
 
-      binding.pry
-      if @company.valid?
-        @company.save
-        session[:name] = params[:company][:name]
-        redirect_to company_path(@company)
-      else
-        @errors = @company.errors.full_messages.to_sentence
-        render :'/companies/signup'
-      end
+    #   binding.pry
+    #   if @company.valid?
+    #     @company.save
+    #     session[:name] = params[:company][:name]
+    #     redirect_to company_path(@company)
+    #   else
+    #     @errors = @company.errors.full_messages.to_sentence
+    #     render :'/companies/signup'
+    #   end
+    # end
+    @company = Company.find_by(name: params[:company][:name])
+    if @company && @company.authenticate(params[:company][:password])
+      session[:name] = params[:company][:name]
+      redirect_to company_path(@company)
     else
-      @company = Company.find_by(name: params[:company][:name])
-      if @company && @company.authenticate(params[:company][:password])
-        session[:name] = params[:company][:name]
-        redirect_to company_path(@company)
-      else
-        @errors = "Invalid combination!"
-        render :'/companies/login'
-      end
+      @errors = "Invalid combination!"
+      render :'/companies/login'
     end
   end
 
--- app/models/company.rb
+++ app/models/company.rb
@@ -6,6 +6,10 @@ class Company < ActiveRecord::Base
   validates :name, uniqueness: true
   validates :motto, uniqueness: true
 
-  def self.find_or_create_by_omniauth(auth_hash)
-  end
+  # def self.find_or_create_by_omniauth(auth_hash)
+  # 	oauth_name = auth["info"]["name"]
+  # 	self.where(:name =>oauth_name).first_or_create do |company|
+  # 		company.password = SecureRandom.hex
+  # 	end
+  # end
 end
