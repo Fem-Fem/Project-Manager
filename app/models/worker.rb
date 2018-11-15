9621971a2ea459081a87791867d6ef73809d14fc
Obafemi Fadairo <obafad94@gmail.com>
2018-11-15 18:24:12 -0500

    Add validations to worker model

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,8 +1,9 @@
 class Worker < ActiveRecord::Base
   belongs_to :company
   belongs_to :project
-  validates_presence_of :name, :position
+  validates_presence_of :name, :position, :rating
   validates :name, uniqueness: true
+  validates :rating, numericality: { only_integer: true }
 
   def self.most_valuable(id)
     where('project_id = ?', id).order(rating: :desc).first
