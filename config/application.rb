48938a1d95695b3ea41aff1b64bd7884eb7e1f7e
Obafemi Fadairo <obafad94@gmail.com>
2018-11-15 19:39:38 -0500

    Working on dotenv

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Dotenv::Railtie.load
 
 module ProjectManager
   class Application < Rails::Application
+    Dotenv.load Rails.root.join('file.env')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
