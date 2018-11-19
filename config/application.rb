93672944dd937e9f4f8b1e39d71dad9c14fb610c
Obafemi Fadairo <obafad94@gmail.com>
2018-11-18 20:21:32 -0500

    Revert omniauth changes

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,9 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
 
 module ProjectManager
   class Application < Rails::Application
-    Dotenv.load Rails.root.join('.env')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
