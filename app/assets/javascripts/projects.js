a022f18b960060cddf50d2ef06992472026779b5
Obafemi Fadairo <obafad94@gmail.com>
2018-11-27 20:05:57 -0500

    Improve workers js functionality

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
 
 const attachEventListeners = () => {
 	$(".workers").on("click", function() {
-		showWorkers();
+		var clicked = wasClicked();
+		if (clicked == true) {
+			showWorkers();
+		}
 	})
 }
 
@@ -12,12 +15,22 @@ function showWorkers() {
 	$.get("/workers", function(data) {
 		data.map( x => [x.name, "/workers/" + x.id])
 		for (var i = 0; i < data.length; i++) {
-			debugger
-			$(".populateWorkers").append(`<a href=${data[i].id}>${data[i].name}</a>`)
 			linebreak = document.createElement("br")
+			$(".populateWorkers").append(`<a href=${data[i].id}>${data[i].name}</a>`)
 			$(".populateWorkers").append(linebreak)
 		}
+		linebreak = document.createElement("br")
+		$(".populateWorkers").append(linebreak)
+		$(".populateWorkers").append(linebreak)
+		$(".populateWorkers").append(linebreak)
 	})
+}
+
+function wasClicked() {
+	if ($(".populateWorkers")[0].children.length == 0) {
+		return true;
+	}
+	return false;
 }
 
 // create function to see if button has been clicked
