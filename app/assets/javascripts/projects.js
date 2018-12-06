593db6c74ca6de005002ffea949cff89f9b71b14
Obafemi Fadairo <obafad94@gmail.com>
2018-12-05 20:28:43 -0500

    Work on project form functionality

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -10,16 +10,23 @@ const attachEventListeners = () => {
 		}
 	})
 
-	$(".NextWorker").on("click", function(e) {
-		e.preventDefault()
-		e.stopPropagation()
+	$(".NextWorker").on("click", function(event) {
+		event.preventDefault();
+		event.stopPropagation();
 		nextWorker();
 	})
 
 	$(".newProject").on("click", function(event) {
 		event.preventDefault();
+		event.stopPropagation();
 		newProject();
 	})
+
+	// $("#companies-project-list").on("click", function(event) {
+	// 	event.preventDefault();
+	// 	newProject();
+	// })
+
 }
 
 function showWorkers() {
@@ -51,15 +58,14 @@ function wasClicked() {
 function nextWorker() {
 	var currentWorker = parseInt($(".NextWorker").attr("worker-id"))
 	$.get("/workers.json", function(data) {
-		var length = data.length
+		var length = data.length;
 		if (data.slice(currentWorker)[0] == null) {
-			var nextWorker = data[0]
+			var nextWorker = data[0];
 		}
 		else {
-			var nextWorker = data.slice(currentWorker)[0]
+			var nextWorker = data.slice(currentWorker)[0];
 		}
 		$.getJSON(`/workers/${nextWorker.id}`, function(data2) {
-			// unsure why this is not working
 			$(".workerName").text(data2["name"])
 			$(".workerPosition").text(data2["position"])
 			$(".workerRating").text(data2["rating"])
@@ -70,17 +76,30 @@ function nextWorker() {
 	})
 }
 
-//
-function newProject() {
-	var values = $(this).serialize
-	var posting = $.post('/projects', values)
+function showManyWorkers() {
 
-	posting.done(function(data) {
-		var project = data;
-		$("#yo").text(project["name"])
-		$("#ya").text(project["description"])	
-	})
 }
 
+function showManyProjects() {
 
-// create check for last worker
+}
+
+function newProject() {
+	var values = $(this).serialize;
+	var posting = $.post('/projects', values);
+
+	data = {
+		'newProject': {
+			'name': $("#project_name").val(),
+			'description': $("#project_description").val()
+		}
+	}
+
+	debugger
+
+	// posting.done(function(data) {
+	// 	var project = data;
+	// 	$("#yo").text(project["name"])
+	// 	$("#ya").text(project["description"])	
+	// })
+}
